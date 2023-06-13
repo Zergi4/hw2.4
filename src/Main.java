0a92a253d25c50cd7be749d4c80daba3f13df6f8
Serg <ksyssau@ya.ru>
2023-06-13 23:17:35 +0300

    проверка логина

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,39 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
+
+
     public static void main(String[] args) {
-
-
+        //   method("ksyssauksyssauksyssau", "k", "y");
+        try {
+            checkLogin("bb*");
+        } catch (WrongLoginException e) {
+            System.out.println(e.getMessage());
+        }
     }
+
+    public static void checkLogin(String login) throws WrongLoginException {
+        // проверка на длину логина
+        if (login.length() > 20) {
+            throw new WrongLoginException("Ошибка! Превышена длина логина");
+        }
+        //проверка на корректность логина
+        char[] loginToChar = login.toCharArray();
+        boolean trueCheck = true;
+        for (int i = 0; i < loginToChar.length; i++) {
+            if (trueCheck) {
+                for (int j = 0; j < allowedChars.length; j++) {
+                    if (loginToChar[i] == allowedChars[j]) {
+                        trueCheck = true;
+                        break;
+                    } else trueCheck = false;
+                    System.out.println(trueCheck);
+                }
+            }
+            if (!trueCheck) {
+                throw new WrongLoginException("Символы не корректны");
+            }
+        }
+    }
+
+    public static char[] allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_".toCharArray();
 }
